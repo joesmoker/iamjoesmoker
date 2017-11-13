133445a4aa91001a6fcb96f7da57f36efb1a38e2
joesmoker <33615774+joesmoker@users.noreply.github.com>
2017-11-13 12:47:06 +0700

    Add files via upload

---
+++ hai.js
@@ -0,0 +1,7 @@
+var nama;
+ nama=prompt('boleh di coba');
+ if(nama){
+  alert('Apa kabar 'semoga beruntung'! Saya Javascript, senang berkenalan dengan Anda');
+ }else{
+  alert('ya semoga beruntung.');
+}
